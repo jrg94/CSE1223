c573a35ba4d9c7bb0dfff57866df9bb6def3739a
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-27 22:05:47 -0400

    Added JUnit Test File

---
+++ Tools/Project01/Project1Test.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+
+/**
+ * Tests Project 1:
+ * http://web.cse.ohio-state.edu/cse1223/currentsem/projects/CSE1223Project01.html
+ */
+public class Project1Test extends TestCase {
+  
+  /**
+   * Verifies the output of the main method.
+   */
+  public void testMain() {
+  }
+  
+}
