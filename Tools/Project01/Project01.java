0da4c40091830942201c2e21d9bc9e58ba8a0b32
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-27 22:29:57 -0400

    Implemented the basic solution

--- Tools/Project01/Project01.java
+++ Tools/Project01/Project01.java
@@ -9,9 +9,35 @@ public class Project01 {
     return num;
   }
   
+  public static void outputArithmetic(int firstVal, int secondVal, char operator) {
+    int result = 0;
+    
+    switch (operator) {
+      case '+':
+        result = firstVal + secondVal;
+        break;
+      case '-':
+        result = firstVal - secondVal;
+        break;
+      case '*':
+        result = firstVal * secondVal;
+        break;
+      case '/':
+        result = firstVal / secondVal;
+        break;
+      case '%':
+        result = firstVal / secondVal;
+        break;
+      default: throw new ArithmeticException();
+    }
+    
+    System.out.println(firstVal + " " + operator + " " + secondVal + " = " + result);
+  }
+  
   public static void main(String[] args) {
     int firstNumber = getNumberFromCommandLine("Enter the first number: ");
     int secondNumber = getNumberFromCommandLine("Enter the first number: ");
+    outputArithmetic(firstNumber, secondNumber, '+');
   }
   
 }
--- Tools/Project01/Project01Test.java
+++ Tools/Project01/Project01Test.java
@@ -1,14 +1,8 @@
+// http://web.cse.ohio-state.edu/cse1223/currentsem/projects/CSE1223Project01.html
 import junit.framework.TestCase;
 
-/**
- * Tests Project 1:
- * http://web.cse.ohio-state.edu/cse1223/currentsem/projects/CSE1223Project01.html
- */
 public class Project01Test extends TestCase {
-  
-  /**
-   * Verifies the output of the main method.
-   */
+
   public void testMain() {
   }
   
