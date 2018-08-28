f56bdb172c4dd94eaf2cfe84ffe1840e94c35372
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-27 22:18:50 -0400

    Refactored the solution

--- Tools/Project01/Project01.java
+++ Tools/Project01/Project01.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 
 public class Project01 {
   
-  public static void main(String[] args0 {
-    System.out.println("Enter the first number: ");
+  public static int getNumberFromCommandLine(String request) {
+    System.out.println(request);
     Scanner keyboard = new Scanner(System.in);
-    int firstNumber = Integer.parseInt(keyboard.nextLine());
-    
+    int num = Integer.parseInt(keyboard.nextLine());
+    return num;
+  }
+  
+  public static void main(String[] args) {
+    int firstNumber = getNumberFromCommandLine("Enter the first number: ");
+    int secondNumber = getNumberFromCommandLine("Enter the first number: ");
   }
   
 }
