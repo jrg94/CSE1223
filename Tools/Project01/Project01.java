5d00f67e077fd38c81c29db686d3758d6f8e3f07
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-27 22:16:00 -0400

    Implementing solution

---
+++ Tools/Project01/Project01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Project01 {
+  
+  public static void main(String[] args0 {
+    System.out.println("Enter the first number: ");
+    Scanner keyboard = new Scanner(System.in);
+    int firstNumber = Integer.parseInt(keyboard.nextLine());
+    
+  }
+  
+}
--- Tools/Project01/Project01Test.java
+++ Tools/Project01/Project01Test.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
  * Tests Project 1:
  * http://web.cse.ohio-state.edu/cse1223/currentsem/projects/CSE1223Project01.html
  */
-public class Project1Test extends TestCase {
+public class Project01Test extends TestCase {
   
   /**
    * Verifies the output of the main method.
