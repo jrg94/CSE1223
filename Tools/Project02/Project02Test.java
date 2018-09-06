993c0b88bb75e1b3fde641fec92505d7f15ec49d
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-09-05 21:53:26 -0400

    Added some helper methods

--- Tools/Project02/Project02Test.java
+++ Tools/Project02/Project02Test.java
@@ -10,15 +10,50 @@ public class Project02Test {
   private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
   private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
   
+  /**
+   * Sets input and output streams to local print streams for analysis.
+   */
   @Before
   public void setUp() {
     System.setOut(new PrintStream(outContent));
     System.setErr(new PrintStream(errContent));
   }
   
+  /**
+   * Sets input and output streams back to normal.
+   */
   @After
   public void tearDown() {
     System.setIn(System.in);
     System.setOut(System.out);
+  }
+  
+  /**
+   * Generates the solution for testing.
+   */
+  public String buildSolution(String longString, String substring, int position, String replacement) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Enter a long string: ");
+    sb.append("Enter a substring: ");
+    return sb.toString();
+  }
+  
+  /**
+   * Constructs user input for testing.
+   */
+  public String constructInput(String longString, String substring, int position, String replacement) {
+    return buildLines(longString, substring, Integer.toString(position), replacement);
+  }
+  
+  /**
+   * Takes a set of inputs and joins them with newlines.
+   */
+  public String buildLines(String ... inputs) {
+    StringBuilder sb = new StringBuilder();
+    for (String input: inputs) {
+      sb.append(input);
+      sb.append(System.lineSeparator());
+    }
+    return sb.toString();
   }
 }
