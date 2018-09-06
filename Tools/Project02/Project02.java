7af2d11712f9f30569bb4ec7979962c04cf4450f
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-09-05 20:06:45 -0400

    Slowly implementing the solution

---
+++ Tools/Project02/Project02.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Project02 {
+  
+  public static void main(String[] args) { 
+//    Enter a long string: The quick brown fox jumped over the lazy dog
+//    Enter a substring: jumped
+//    Length of your string: 44
+//    Length of your substring: 6
+//    Starting position of your substring in string: 20
+//    String before your substring: The quick brown fox
+//    String after your substring:  over the lazy dog
+//    Enter a position between 0 and 43: 18
+//    The character at position 18 is x
+//    Enter a replacement string: leaped
+//    Your new string is: The quick brown fox leaped over the lazy dog
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter a long string: ");
+    String fullString = in.nextLine();
+    System.out.println("Enter a substring: ");
+    String subString = in.nextLine();
+    
+  }
+   
+}
